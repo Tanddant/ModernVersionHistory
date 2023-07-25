d568487639e4d1ece48e2c9b2dd6f4923f75b7ab
Dan Toft <tanddant@gmail.com>
2023-07-25 19:28:43 +0200

    🎉 - Init repo

---
+++ src/extensions/betterVersionHistory/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IBetterVersionHistoryCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'BetterVersionHistoryCommandSetStrings' {
+  const strings: IBetterVersionHistoryCommandSetStrings;
+  export = strings;
+}
