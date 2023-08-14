8745ca84ba2d192bf7f4bb4225076f9ab2f36def
Dan Toft <Tanddant@gmail.com>
2023-08-14 20:48:35 +0200

    🔨 - Moved the parser to another file

--- src/extensions/betterVersionHistory/providers/DataProvider.ts
+++ src/extensions/betterVersionHistory/providers/DataProvider.ts
@@ -2,8 +2,7 @@ import { ListViewCommandSetContext } from "@microsoft/sp-listview-extensibility"
 import { SPFx, SPFI, spfi } from "@pnp/sp/presets/all";
 import { IField } from "../models/IField";
 import { IVersion } from "../models/IVersion";
-import { DisplayFormat, FieldType } from "../models/FieldTypes";
-import { IFieldLookupValue, IFieldUrlValue, ITaxonomyFieldValue } from "../models/FieldValues";
+import { GetChanges } from "../models/FieldValues";
 
 export interface IDataProvider {
     GetVersions(): Promise<IVersion[]>
@@ -23,6 +22,7 @@ export class DataProvider implements IDataProvider {
         return this._SPFI;
     }
 
+    
     private fieldsToSkip: string[] = ["Modified", "Created"];
     public async GetVersions(): Promise<IVersion[]> {
         const fields = await this.GetFields(this._context.pageContext.list.id.toString());
@@ -31,7 +31,6 @@ export class DataProvider implements IDataProvider {
 
         const Changes: IVersion[] = [];
 
-        const fieldsToHandle: string[] = [];
         for (let i = versions.length; i > 0; i--) {
             const version = versions[i - 1];
             const prevVersion = versions[i] ?? {};
@@ -47,143 +46,9 @@ export class DataProvider implements IDataProvider {
                 if (this.fieldsToSkip.indexOf(field.StaticName) !== -1)
                     continue;
 
-                switch (field.TypeAsString) {
-                    case FieldType.Text:
-                    case FieldType.Note:
-                    case FieldType.Integer:
-                    case FieldType.Number:
-                    case FieldType.Choice:
-                        if (version[field.StaticName] !== prevVersion[field.StaticName]) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: prevVersion[field.StaticName],
-                                NewValue: version[field.StaticName],
-                                FieldType: field.TypeAsString
-                            });
-                        }
-                        break;
-                    case FieldType.Lookup:
-                    case FieldType.User:
-                        if ((version[field.StaticName] as IFieldLookupValue)?.LookupId !== (prevVersion[field.StaticName] as IFieldLookupValue)?.LookupId) {
-                            const link = new URL(this._context.pageContext.web.absoluteUrl)
-                            link.pathname += "/_layouts/15/listform.aspx";
-                            link.searchParams.append("PageType", "4");
-                            link.searchParams.append("ListId", field.LookupList);
-                            link.searchParams.append("ID", (version[field.StaticName] as IFieldLookupValue)?.LookupId.toString());
-                            link.searchParams.append("RootFolder", "*");
-
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: (prevVersion[field.StaticName] as IFieldLookupValue)?.LookupValue,
-                                NewValue: (version[field.StaticName] as IFieldLookupValue)?.LookupValue,
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName],
-                                Link: link.toString()
-                            });
-                        }
-                        break;
-                    case FieldType.DateTime:
-                        if (new Date(version[field.StaticName]).toLocaleString() !== new Date(prevVersion[field.StaticName]).toLocaleString()) {
-                            if (field.DisplayFormat === DisplayFormat.DateOnly) {
-                                Version.Changes.push({
-                                    FieldName: field.Title,
-                                    FieldInternalName: field.StaticName,
-                                    OldValue: new Date(prevVersion[field.StaticName]).toLocaleDateString(),
-                                    NewValue: new Date(version[field.StaticName]).toLocaleDateString(),
-                                    FieldType: field.TypeAsString
-                                });
-                            } else {
-                                Version.Changes.push({
-                                    FieldName: field.Title,
-                                    FieldInternalName: field.StaticName,
-                                    OldValue: new Date(prevVersion[field.StaticName]).toLocaleString(),
-                                    NewValue: new Date(version[field.StaticName]).toLocaleString(),
-                                    FieldType: field.TypeAsString
-                                });
-                            }
-                        }
-                        break;
-                    case FieldType.UserMulti:
-                    case FieldType.LookupMulti:
-                        if (JSON.stringify(version[field.StaticName]) !== JSON.stringify(prevVersion[field.StaticName])) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: (prevVersion[field.StaticName] as IFieldLookupValue[])?.map(x => x.LookupValue).join("\n"),
-                                NewValue: (version[field.StaticName] as IFieldLookupValue[])?.map(x => x.LookupValue).join("\n"),
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName]
-                            });
-                        }
-                        break;
-                    case FieldType.MultiChoice:
-                        if (JSON.stringify(version[field.StaticName]) !== JSON.stringify(prevVersion[field.StaticName])) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: (prevVersion[field.StaticName] as string[])?.join("\n"),
-                                NewValue: (version[field.StaticName] as string[])?.join("\n"),
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName]
-                            });
-                        }
-                        break;
-                    case FieldType.URL: {
-                        const BeforeUrlString = `${(prevVersion[field.StaticName] as IFieldUrlValue)?.Description} (${(prevVersion[field.StaticName] as IFieldUrlValue)?.Url})`;
-                        const NewUrlString = `${(version[field.StaticName] as IFieldUrlValue).Description} (${(version[field.StaticName] as IFieldUrlValue).Url})`;
-                        if (BeforeUrlString !== NewUrlString) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: BeforeUrlString,
-                                NewValue: NewUrlString,
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName]
-                            });
-                        }
-                        break;
-                    }
-                    case FieldType.Boolean:
-                        if (version[field.StaticName] !== prevVersion[field.StaticName]) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: prevVersion[field.StaticName] ? "Yes" : "No",
-                                NewValue: version[field.StaticName] ? "Yes" : "No",
-                                FieldType: field.TypeAsString
-                            });
-                        }
-                        break;
-                    case FieldType.Taxonomy:
-                        if (JSON.stringify(version[field.StaticName]) !== JSON.stringify(prevVersion[field.StaticName])) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: (prevVersion[field.StaticName] as ITaxonomyFieldValue)?.Label,
-                                NewValue: (version[field.StaticName] as ITaxonomyFieldValue)?.Label,
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName]
-                            });
-                        }
-                        break;
-                    case FieldType.TaxonomyMulti:
-                        if (JSON.stringify(version[field.StaticName]) !== JSON.stringify(prevVersion[field.StaticName])) {
-                            Version.Changes.push({
-                                FieldName: field.Title,
-                                FieldInternalName: field.StaticName,
-                                OldValue: (prevVersion[field.StaticName] as ITaxonomyFieldValue[])?.map(x => x.Label).join("\n"),
-                                NewValue: (version[field.StaticName] as ITaxonomyFieldValue[])?.map(x => x.Label).join("\n"),
-                                FieldType: field.TypeAsString,
-                                Data: version[field.StaticName]
-                            });
-                        }
-                        break;
-                    default:
-                        if (fieldsToHandle.indexOf(field.TypeAsString) === -1)
-                            fieldsToHandle.push(field.TypeAsString);
-                }
+                const change = GetChanges(field, version, prevVersion);
+                if (change !== undefined)
+                    Version.Changes.push(change);
             }
 
             Changes.push(Version);
@@ -197,8 +62,4 @@ export class DataProvider implements IDataProvider {
     private async GetFields(listId: string): Promise<IField[]> {
         return this.getSPFI().web.lists.getById(listId).fields.filter("Hidden eq false")();
     }
-
-
-
-
 }
