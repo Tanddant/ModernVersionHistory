c983bc04128ad49e8b88f3b717d839ef72d13d9e
Tobias Maestrini <tobias@bee365.ch>
2023-09-28 22:30:23 +0200

    fix minor linting issues

--- src/extensions/betterVersionHistory/hooks/useVersionMetadata.ts
+++ src/extensions/betterVersionHistory/hooks/useVersionMetadata.ts
@@ -6,7 +6,7 @@ import { IVersion } from '../models/IVersion';
 export default function useVersionMetadata(version: IVersion, provider: IDataProvider): { versionMetadata: IFileInfo } {
   const [versionMetadata, setVersionMetadata] = React.useState<IFileInfo>(undefined);
 
-  async function getMetadata() {
+  async function getMetadata(): Promise<void> {
     const { FileRef, VersionId } = version;
     const metadata = await provider.GetFileVersionMetadata(FileRef, VersionId);
     setVersionMetadata(metadata);
